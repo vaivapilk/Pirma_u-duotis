73c90d9f78fd0700854a68ec4690fab70438b3c8
vaivapilk <56174822+vaivapilk@users.noreply.github.com>
2021-09-23 20:21:51 +0300

    Update

--- v0.1.cpp
+++ v0.1.cpp
@@ -18,148 +18,144 @@ using std::regex;
 using std::regex_match;
 
 struct studentas {
-    string vard, pavard;
-    //nusprendziau, kad pazymiai gali buti tik sveikieji skaiciai
-    int* nd = new(std::nothrow) int[5];
-    int egz;
-    float gal;
-    float median;
+	string vard, pavard;
+	//nusprendziau, kad pazymiai gali buti tik sveikieji skaiciai
+	int nd[10];
+	int egz;
+	float gal;
+	float median;
 };
 
 void clearInput() {
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	std::cin.clear();
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
 //tikrinam, ar tai sveikasis skaicius intervale [1;10], naudoju REGEX, kad tikrai nepraeitu tokie issireiskimai kaip "10aaa"
 void tikrinam_regex(int& x) {
-    string input;
-    regex integer("^([1-9]|1[0])$");
-    while (true)
-    {
-        cin >> input;
-        if (regex_match(input, integer))
-            break;
-        else
-        {
-            cout << "Ivestis klaidinga! Pabandykite is naujo " << endl;
-            clearInput();
-        }
-    }
-    x = std::stoi(input);
+	string input;
+	regex integer("^([1-9]|1[0])$");
+	while (true)
+	{
+		cin >> input;
+		if (regex_match(input, integer))
+			break;
+		else
+		{
+			cout << "Ivestis klaidinga! Pabandykite is naujo " << endl;
+			clearInput();
+		}
+	}
+	x = std::stoi(input);
 }
 
 void pild(studentas& kint) {
-    int n;
-    string nn;
-    cout << "Iveskite studento varda ir pavarde: "; cin >> kint.vard >> kint.pavard;
-    cout << "Iveskite pazymius, kai nuspresite, kad jau ivedete visus (iki 10 pazymiu), parasykite 'q' " << endl;
+	int n;
+	cout << "Iveskite studento varda ir pavarde: "; cin >> kint.vard >> kint.pavard;
+	cout << "Iveskite pazymius, kai nuspresite, kad jau ivedete visus (iki 10 pazymiu), parasykite 'q' " << endl;
 
-    float sum = 0, vid = 0, med = 0;
+	float sum = 0, vid = 0, med = 0;
 
-    string input;
-    regex integer("^([1-9]|1[0])$");
-    int i = 0;
-    while (true)
-    {
-        cin >> input;
-        if (input == "q")
-            break;
-        if (regex_match(input, integer))
-        {
-            kint.nd[i] = std::stoi(input);
-            sum += kint.nd[i];
-            i++;
-        }
-        else
-        {
-            cout << "Ivestis klaidinga! Pabandykite is naujo " << endl;
-            clearInput();
-        }
-        //if(!kint.nd) cout << "allocation of memory failed\n";
-        
-    }
-    n = i;
-   
-    vid = sum / n;
+	string input;
+	regex integer("^([1-9]|1[0])$");
+	int i = 0;
+	while (true)
+	{
+		cin >> input;
+		if (input == "q")
+			break;
+		if (regex_match(input, integer))
+		{
+			kint.nd[i] = std::stoi(input);
+			sum += kint.nd[i];
+			i++;
+		}
+		else
+		{
+			cout << "Ivestis klaidinga! Pabandykite is naujo " << endl;
+			clearInput();
+		}
 
-    cout << "Iveskite egzamino pazymi: " << endl;
-    tikrinam_regex(kint.egz);
-    kint.gal = vid * 0.4 + kint.egz * 0.6;
+	}
+	n = i;
 
-    //medianos skaiciavimas
-    sort(kint.nd, kint.nd + n);
-    if (n % 2 != 0)
-        kint.median = kint.nd[n / 2];
-    else
-        kint.median = (kint.nd[(n - 1) / 2] + kint.nd[n / 2]) / 2.0;
+	vid = sum / n;
+
+	cout << "Iveskite egzamino pazymi: " << endl;
+	tikrinam_regex(kint.egz);
+	kint.gal = vid * 0.4 + kint.egz * 0.6;
+
+	//medianos skaiciavimas
+	sort(kint.nd, kint.nd + n);
+	if (n % 2 != 0)
+		kint.median = kint.nd[n / 2];
+	else
+		kint.median = (kint.nd[(n - 1) / 2] + kint.nd[n / 2]) / 2.0;
 }
 
 void printas(studentas& kin) {
-    cout << setw(15) << left << kin.pavard << setw(15) << kin.vard;
+	cout << setw(15) << left << kin.pavard << setw(15) << kin.vard;
 }
 
 
 int main()
 {
-    int stud_kiek;
-    string input;
-    regex integer("^[1-9]$|^[1-9][0-9]$|^(100)$"); //patikriname, kad butu ivestas skaicius tarp 1-100 studentu skaiciui
-    while (true)
-    {
-        cout << "Iveskite, kiek bus studentu: " << endl;
-        cin >> input;
-        if (regex_match(input, integer))
-            break;
-        else
-        {
-            cout << "Ivestis klaidinga, bandykite is naujo " << endl;
-            clearInput();
-        }
-    }
-    stud_kiek = std::stoi(input);
+	int stud_kiek;
+	string input;
+	regex integer("^[1-9]$|^[1-9][0-9]$|^(100)$"); //patikriname, kad butu ivestas skaicius tarp 1-100 studentu skaiciui
+	while (true)
+	{
+		cout << "Iveskite, kiek bus studentu: " << endl;
+		cin >> input;
+		if (regex_match(input, integer))
+			break;
+		else
+		{
+			cout << "Ivestis klaidinga, bandykite is naujo " << endl;
+			clearInput();
+		}
+	}
+	stud_kiek = std::stoi(input);
 
-    studentas studentai[100]; //nusprendziau, kad max gali buti 100 studentu
-    for (int i = 0; i < stud_kiek; i++)
-        pild(studentai[i]);
+	studentas studentai[100]; //nusprendziau, kad max gali buti 100 studentu
+	for (int i = 0; i < stud_kiek; i++)
+		pild(studentai[i]);
 
-    //medianos arba vidurkio uzklausa bei tikrinimas, kad ivesti teisingi simboliai
-    char uzk;
-    while (true)
-    {
-        cout << "Jei norite, kad butu suskaiciuotas vidurkis, parasykite 'v', o jei norite medianos - 'm'" << endl;
-        cin >> uzk;
-        if (uzk == 'v' || uzk == 'm'){
-            break;
-        }
-        else {
-            cout << "Ivestis klaidinga, bandykite is naujo " << endl;
-            clearInput();
-        }
-    }
+	//medianos arba vidurkio uzklausa bei tikrinimas, kad ivesti teisingi simboliai
+	char uzk;
+	while (true)
+	{
+		cout << "Jei norite, kad butu suskaiciuotas vidurkis, parasykite 'v', o jei norite medianos - 'm'" << endl;
+		cin >> uzk;
+		if (uzk == 'v' || uzk == 'm') {
+			break;
+		}
+		else {
+			cout << "Ivestis klaidinga, bandykite is naujo " << endl;
+			clearInput();
+		}
+	}
 
-    //isvedimas
-    cout << setw(15) << left << "Pavarde " << setw(15) << "Vardas ";
+	//isvedimas
+	cout << setw(15) << left << "Pavarde " << setw(15) << "Vardas ";
 
-    if (uzk == 'v'){
-        cout << setw(10) << "Galutinis (Vid.) " << endl;
-    }
-    if (uzk == 'm'){
-        cout << setw(10) << "Galutinis (Med.) " << endl;
-    }
+	if (uzk == 'v') {
+		cout << setw(10) << "Galutinis (Vid.) " << endl;
+	}
+	if (uzk == 'm') {
+		cout << setw(10) << "Galutinis (Med.) " << endl;
+	}
 
-    
-    for (int i = 0; i < 50; i++)
-        cout << "-";
-    cout << endl;
-    for (int i = 0; i < stud_kiek; i++) {
-        printas(studentai[i]);
-        if (uzk == 'v'){
-            cout << setw(10) << left << fixed << setprecision(2) << studentai[i].gal << endl;
-        }
-        if (uzk == 'm') {
-            cout << setw(10) << left << fixed << setprecision(2) << studentai[i].median << endl;
-        }
-       delete[] studentai[i].nd;
-    }
+	for (int i = 0; i < 50; i++)
+		cout << "-";
+	cout << endl;
+	for (int i = 0; i < stud_kiek; i++) {
+		printas(studentai[i]);
+		if (uzk == 'v') {
+			cout << setw(10) << left << fixed << setprecision(2) << studentai[i].gal << endl;
+		}
+		if (uzk == 'm') {
+			cout << setw(10) << left << fixed << setprecision(2) << studentai[i].median << endl;
+		}
+	}
 }
