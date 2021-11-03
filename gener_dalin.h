e888539bc32e9addfb69195563323854ad33a226
vaivapilk <56174822+vaivapilk@users.noreply.github.com>
2021-11-03 12:20:22 +0200

    Add files via upload

---
+++ gener_dalin.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "struct.h"
+
+void generavimas(int K);
+void dalinimas(char uzk, list<studentas>& grupe, list<studentas>& moksliukai, list<studentas>& nesimoke);
---
+++ nuskaitymas_is_failo.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "struct.h"
+
+void skaitymas(studentas& kint, int paz_sk);
+int is_failo(list<studentas>& grupe);
+
